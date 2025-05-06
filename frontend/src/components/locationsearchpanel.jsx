1c5b4d0b3937fb83ef3c4f365dc59647e1146ef3
prafullamishra001 <113182387+prafullamishra001@users.noreply.github.com>
2025-05-06 10:30:44 +0530

    choose vehicle done

--- frontend/src/components/locationsearchpanel.jsx
+++ frontend/src/components/locationsearchpanel.jsx
@@ -1,34 +1,44 @@
 import React from 'react'
 
 const LocationSearchPanel = () => {
+// sample array for the location
+  const locations=[
+    "B-179 Rama green city, khamtarai road, bilaspur C.G",
+    "C-173 Natures valley, kargi road, bilaspur C.G",
+    "D-179 Dream city, nutan road, bilaspur C.G",
+    "E-179 Green city, khamtarai road, bilaspur C.G",]
+
+
+
+
 return(
     <div>
 
-    <div className='flex gap-4 items-center justify-start pt-10 my-4'>
+    <div className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center justify-start pt-10 my-2'>
         <h2 className='bg-[#eee] h-8 flex items-center justify-center  w-12 rounded-full'><i className="ri-map-pin-line"></i></h2>
       <h4 className='font-medium'>
         B-179,Rama green city, khamtarai road, bilaspur C.G</h4>  
     </div>
 
-    <div className='flex gap-4 items-center justify-start pt-10 my-4'>
+    <div className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center justify-start pt-10 my-2'>
         <h2 className='bg-[#eee] h-8 flex items-center justify-center  w-12 rounded-full'><i className="ri-map-pin-line"></i></h2>
       <h4 className='font-medium'>
         B-179,Rama green city, khamtarai road, bilaspur C.G</h4>  
     </div>
 
-    <div className='flex gap-4 items-center justify-start pt-10 my-4'>
+    <div className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center justify-start pt-10 my-2'>
         <h2 className='bg-[#eee] h-8 flex items-center justify-center  w-12 rounded-full'><i className="ri-map-pin-line"></i></h2>
       <h4 className='font-medium'>
         B-179,Rama green city, khamtarai road, bilaspur C.G</h4>  
     </div>
 
-    <div className='flex gap-4 items-center justify-start pt-10 my-4'>
+    <div className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center justify-start pt-10 my-2'>
         <h2 className='bg-[#eee] h-8 flex items-center justify-center  w-12 rounded-full'><i className="ri-map-pin-line"></i></h2>
       <h4 className='font-medium'>
         B-179,Rama green city, khamtarai road, bilaspur C.G</h4>  
     </div>
 
-    <div className='flex gap-4 items-center justify-start pt-10 my-4'>
+    <div className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center justify-start pt-10 my-2'>
         <h2 className='bg-[#eee] h-8 flex items-center justify-center  w-12 rounded-full'><i className="ri-map-pin-line"></i></h2>
       <h4 className='font-medium'>
         B-179,Rama green city, khamtarai road, bilaspur C.G</h4>  
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -4,6 +4,7 @@ import {gsap} from 'gsap'
 import 'remixicon/fonts/remixicon.css'
 import LocationSearchPanel from './../components/locationsearchpanel';
 
+
 const Home=() => { 
     const [pickUp,setPickUp]=useState('')
     const [destination,setdestination]=useState('')
@@ -40,7 +41,7 @@ e.preventDefault();
     },[panelopen])
 
     return(
-        <div className='h-screen relative'>
+        <div className='h-screen overflow-hidden'>
             <img className='w-16 absolute mt-5 ml-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
         <div className='h-screen w-screen'>
             <img className='h-full w-full object-cover 'src="https://www.medianama.com/wp-content/uploads/2018/06/Screenshot_20180619-112715.png.png" alt="" />
@@ -82,6 +83,40 @@ e.preventDefault();
            <div ref={panelRef} className='bg-white h-0'>
             <LocationSearchPanel/>
            </div>
+        </div>
+        <div className='fixed w-full z-10 bottom-0 translate-y-full px-3 py-8 bg-white'>
+        <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
+            
+               <div className='flex border-2 border-white active:border-black  mb-2 rounded-xl w-full p-3 items-center justify-between'>
+                <img className='h-15' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png" alt="" />
+                <div className=' w-1/2 ml-4 '>
+                    <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
+                    <h5 className='font-medium text-sm'>2 minutes away</h5>
+                    <p className='font-normal text-xs text-gray-600'>Affordable compact rides</p>
+                </div>
+                <h2 className='text-lg font-semibold'>₹193.20</h2>
+            </div>  
+
+            <div className='flex border-2 border-white active:border-black  mb-2 rounded-xl w-full p-3 items-center justify-between'>
+                <img className='h-15' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1698944322/assets/92/00189a-71c0-4f6d-a9de-1b6a85239079/original/UberMoto-India-Orange.png" alt="" />
+                <div className=' w-1/2 ml-2 '>
+                    <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
+                    <h5 className='font-medium text-sm'>3 minutes away</h5>
+                    <p className='font-normal text-xs text-gray-600'>Affordable motorcycle rides </p>
+                </div>
+                <h2 className='text-lg font-semibold'>₹65</h2>
+            </div> 
+
+              <div className='flex border-2 border-white active:border-black  mb-2 rounded-xl w-full p-3 items-center justify-between'>
+                <img className='h-15' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+                <div className=' w-1/2 ml-8 '>
+                    <h4 className='font-medium text-base'>Uber auto <span><i className="ri-user-3-fill"></i>3</span></h4>
+                    <h5 className='font-medium text-sm'>3 minutes away</h5>
+                    <p className='font-normal text-xs text-gray-600'>Affordable auto rides </p>
+                </div>
+                <h2 className='text-lg font-semibold'>₹118.85</h2>
+            </div>  
+
         </div>
         </div>
     )
