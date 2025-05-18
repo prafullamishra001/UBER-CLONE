d322c3e3c216a1d95a697657be1f72da14aa77b0
prafullamishra001 <113182387+prafullamishra001@users.noreply.github.com>
2025-05-18 14:03:38 +0530

    fare calculation done

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,7 @@ const captainRoutes=require('./routes/captain.routes');
 const { cookie } = require('express-validator');
 const cookieParser=require('cookie-parser');
 const mapRoutes=require('./routes/maps.routes');
+const rideRoutes=require('./routes/rides.routes');
 
 
 connectToDb();
@@ -22,5 +23,6 @@ app.get('/',(req,res)=>{
 app.use('/users',userRoutes);
 app.use('/captains',captainRoutes);
 app.use('/maps',mapRoutes);
+app.use('/rides',rideRoutes);
 
 module.exports=app;
---
+++ Backend/services/ride.service.js
@@ -0,0 +1,57 @@
+const ridemodel = require('../models/ride.model');
+const mapService = require('./maps.service');
+
+async function getFare(pickup,destination){
+   if(!pickup || !destination) {
+        throw new Error('Pickup and destination are required');
+    }
+    const distanceTime =await mapService.getDistanceTime(pickup, destination);
+
+    const baseFare={
+auto:30,
+car:50,
+motorcycle:20
+    };
+
+    const perKmRate={
+auto:10,
+car:15, 
+motorcycle:8    
+    }
+
+    const perMinuteRate={
+auto:2,
+car:3,  
+motorcycle:1.5
+    }
+
+    console.log(distanceTime);
+    const fare={
+        auto:baseFare.auto + ((distanceTime.distance.value/1000) * perKmRate.auto) + ((distanceTime.duration.value/60) * perMinuteRate.auto),
+        car:baseFare.car + ((distanceTime.distance.value/1000) * perKmRate.car) + ((distanceTime.duration.value/60) * perMinuteRate.car),
+        motorcycle:baseFare.motorcycle + ((distanceTime.distance.value/1000) * perKmRate.motorcycle) + ((distanceTime.duration.value/60) * perMinuteRate.motorcycle)
+
+    };
+    return fare;
+   
+
+}
+
+
+module.exports.createRide = async ({
+    user,pickup,destination,vehicleType
+}) => {
+    if(!user || !pickup || !destination || !vehicleType){
+        throw new Error('All fields are required');
+    }
+    const fare = await getFare(pickup, destination);
+    const ride = ridemodel.create({
+        user,
+        pickup,
+        destination,
+        fare: fare[vehicleType],
+    
+    });
+    return ride;
+}
+
